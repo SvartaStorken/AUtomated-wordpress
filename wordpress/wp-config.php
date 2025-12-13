f93de29e516b9c7ff49951eb9ba762d9787949d2
Martin Wallin <martin_junkan@hotmail.com>
2025-12-13 23:57:43 +0100

    fix: Configure PHP-FPM to pass env vars and revert to dynamic config

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -2,12 +2,12 @@
 // Läs in inställningar från OpenShift Secrets (Env vars)
 define( 'DB_NAME',     getenv('DB_NAME') ?: 'wordpress_db' );
 define( 'DB_USER',     getenv('DB_USER') ?: 'wordpress' );
-define( 'DB_PASSWORD', getenv('DB_PASSWORD') ?: 'password' );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
 define( 'DB_HOST',     'mariadb' ); // Namnet på din Service
 define( 'DB_CHARSET',  'utf8' );
 define( 'DB_COLLATE',  '' );
 
-// Unika nycklar och salter (Viktigt för säkerhet!)
+// Säkerhetsnycklar (Valfritt men bra)
 define('AUTH_KEY',         getenv('WP_AUTH_KEY'));
 define('SECURE_AUTH_KEY',  getenv('WP_SECURE_AUTH_KEY'));
 define('LOGGED_IN_KEY',    getenv('WP_LOGGED_IN_KEY'));
@@ -20,11 +20,8 @@ define('NONCE_SALT',       getenv('WP_NONCE_SALT'));
 $table_prefix = 'wp_';
 
 define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', true );
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
-
 require_once ABSPATH . 'wp-settings.php';
